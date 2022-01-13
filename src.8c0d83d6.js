a3d3dc42bf31b1cc8ae55d5901f010443b81a59b
Finkons <91851050+Finkons@users.noreply.github.com>
2022-01-13 09:42:51 +0000

    Deploying to gh-pages from @ Finkons/team-project-icecream@7282444eef7d5c76c9c933fffd6fefecb14a5d98 🚀

--- src.8c0d83d6.js
+++ src.8c0d83d6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-icecream/src.4a5c56a0.js.map
+//# sourceMappingURL=/team-project-icecream/src.8c0d83d6.js.map
